5778933d58b237d2292b2cddf59e1a5ee6199d0f
Nicolás Cerrini <ceneon@gmail.com>
2014-01-06 13:18:05 -0200

    Reset timer when next() or prev()
    It uses .autoplay as a flag now.
    It may have errors :P

--- minimal-carousel.js
+++ minimal-carousel.js
@@ -14,13 +14,20 @@ function Carousel(settings){
   }
 }
 
-Carousel.prototype.next = function () {
+Carousel.prototype.next = function (is_interval_call) {
   'use strict';
   for (var s = 0; s < this.slides.length; s += 1) {
     this.slides[s].style.display = 'none';
   }
   this.current_slide = (this.current_slide + 1) % this.slides.length;
   this.slides[this.current_slide].style.display = 'block';
+  if (this.autoplay && this.interval && !is_interval_call) {
+    var that = this;
+    clearInterval(this.interval);
+    this.interval = setTimeout(function () {
+      that.play();
+    }, this.delay * 1000);
+  }
 };
 
 Carousel.prototype.prev = function () {
@@ -30,23 +37,29 @@ Carousel.prototype.prev = function () {
   }
   this.current_slide = Math.abs(this.current_slide - 1) % this.slides.length;
   this.slides[this.current_slide].style.display = 'block';
-};
-
-Carousel.prototype.play = function () {
-  'use strict';
-  this.next();
-  var that = this;
-  if (this.autoplay) {
+  if (this.autoplay && this.interval) {
+    var that = this;
+    clearInterval(this.interval);
     this.interval = setTimeout(function () {
       that.play();
     }, this.delay * 1000);
   }
 };
 
+Carousel.prototype.play = function () {
+  'use strict';
+  this.next(true);
+  var that = this;
+  this.autoplay = true;
+  this.interval = setTimeout(function () {
+    that.play();
+  }, this.delay * 1000);
+};
+
 Carousel.prototype.stop = function () {
   'use strict';
   if (this.interval) {
+    this.autoplay = false;
     clearInterval(this.interval);
   }
 };
-
